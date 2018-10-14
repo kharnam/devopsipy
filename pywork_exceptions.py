53f45075bfe25fac27f5ac820d362e1e5e020e1d
Sergey Kharnam <kharnam@gmail.com>
2018-10-14 17:31:48 -0400

    upd: GenericException --> PyworkException; add: log.debug in PyworkException

--- pywork_exceptions.py
+++ pywork_exceptions.py
@@ -8,10 +8,11 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class GenericException(Exception):
+class PyworkException(Exception):
     """
     Represents generic exception with custom message
     """
     def __init__(self, message, errors=None):
         super().__init__(message)
         self.errors = errors
+        log.exception(message)
