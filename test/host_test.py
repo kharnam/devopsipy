3b390992ad45e10c6fa14b4b7fb03ce2c95a1ddc
Sergey Kharnam <kharnam@gmail.com>
2018-10-14 21:53:18 -0400

    upd: small change

--- test/host_test.py
+++ test/host_test.py
@@ -8,7 +8,7 @@ def main():
     log = logger.set_logger('HostBaseTest')
     log.info('Running HostBase Test...')
     host = host_base.HostBase(hostname='18.232.148.79', ssh_user='ec2-user', ssh_key_file='/Users/kharnam/.ssh/id_rsa')
-    host.run(commands=['uptime', 'ls -l'])
+    host.run(commands=['uptime\n', 'ls -l\n'])
 
 
 if __name__ == '__main__':
