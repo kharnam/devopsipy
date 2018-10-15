b4db503010e5b1ad199a6bde9ebe2c5a6c683a15
Sergey Kharnam <kharnam@gmail.com>
2018-10-15 00:19:24 -0400

    add: HostCommandExecutionError

--- pywork_exceptions.py
+++ pywork_exceptions.py
@@ -26,3 +26,13 @@ class HostConnectivityError(Exception):
         super().__init__(message)
         self.errors = errors
         log.exception(message)
+
+
+class HostCommandExecutionError(Exception):
+    """
+    Host command execution exception
+    """
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+        log.exception(message)
