fff3a4d2ef998f21bd2330396e1ea03945d3d27a
Sergey Kharnam <kharnam@gmail.com>
2018-10-15 00:19:01 -0400

    fix: small fix

--- test/host_test.py
+++ test/host_test.py
@@ -7,8 +7,18 @@ import host_base
 def main():
     log = logger.set_logger('HostBaseTest')
     log.info('Running HostBase Test...')
-    host = host_base.HostBase(hostname='18.232.148.79', ssh_user='ec2-user', ssh_key_file='/Users/kharnam/.ssh/id_rsa')
-    host.run(commands=['uptime\n', 'ls -l\n'])
+    # host = host_base.HostBase(hostname='18.232.148.79',
+    #                           ssh_user='ec2-user',
+    #                           ssh_key_file='/Users/kharnam/.ssh/id_rsa',
+    #                           )
+    # p = host.run(commands=['uptime', 'ls -l'],
+    #              print_pstate=True)
+    # print(p.__str__())
+
+    host = host_base.HostBase(hostname='localhost')
+    p = host.run(commands=['uptime', 'ls -l'],
+                 print_pstate=True)
+    print(p.__str__())
 
 
 if __name__ == '__main__':
