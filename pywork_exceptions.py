badd29be76855b5197596ea017659bbec20c5325
Sergey Kharnam <kharnam@gmail.com>
2018-10-14 21:51:37 -0400

    add: HostConnectivityError

--- pywork_exceptions.py
+++ pywork_exceptions.py
@@ -16,3 +16,13 @@ class PyworkException(Exception):
         super().__init__(message)
         self.errors = errors
         log.exception(message)
+
+
+class HostConnectivityError(Exception):
+    """
+    Host connectivity exception
+    """
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+        log.exception(message)
