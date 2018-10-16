0d70238e5f6347d8848257c318e88026506419f4
Sergey Kharnam <kharnam@gmail.com>
2018-10-15 20:19:34 -0400

    upd: host_test

--- test/host_test.py
+++ test/host_test.py
@@ -7,18 +7,18 @@ import host_base
 def main():
     log = logger.set_logger('HostBaseTest')
     log.info('Running HostBase Test...')
-    # host = host_base.HostBase(hostname='18.232.148.79',
-    #                           ssh_user='ec2-user',
-    #                           ssh_key_file='/Users/kharnam/.ssh/id_rsa',
-    #                           )
-    # p = host.run(commands=['uptime', 'ls -l'],
-    #              print_pstate=True)
-    # print(p.__str__())
+    host1 = host_base.HostBase(hostname='18.232.148.79',
+                               ssh_user='ec2-user',
+                               ssh_key_file='/Users/kharnam/.ssh/id_rsa',
+                               )
+    p_lst = host1.run(commands=['uptime', 'ls -l'], print_pstate=True, print_stdout=True)
+    # for p in p_lst:
+    #     print(p.__str__())
 
-    host = host_base.HostBase(hostname='localhost')
-    p = host.run(commands=['uptime', 'ls -l'],
-                 print_pstate=True)
-    print(p.__str__())
+    host2 = host_base.HostBase(hostname='localhost')
+    p_lst = host2.run(commands=['uptime', 'ls -l'], print_pstate=True, print_stdout=True)
+    # for p in p_lst:
+    #     print(p.__str__())
 
 
 if __name__ == '__main__':
